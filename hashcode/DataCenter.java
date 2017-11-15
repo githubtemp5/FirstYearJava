89922ecd76b87ddcd685226aa241a47a7d597c3b
githubtemp5 <30862041+githubtemp5@users.noreply.github.com>
2017-11-15 10:37:52 +0000

    Update DataCenter.java

--- hashcode/DataCenter.java
+++ hashcode/DataCenter.java
@@ -4,6 +4,7 @@ public class DataCenter
     public ArrayList<Video> videos;
     public DataCenter()
     {
+        videos = new ArrayList<Video>();
     }
     
     public void addVideo(Video v){
